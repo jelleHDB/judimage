c11de9afcbcf36cb0736ec0d440f3d20b984ce2c
Jelle Bol <jelle@helemaaldebom.nl>
2024-03-04 17:29:21 +0100

    Homepagina ZONDER Javascript effecten af

--- wp-content/themes/blankslate-child/Pagina templates/home-page.php
+++ wp-content/themes/blankslate-child/Pagina templates/home-page.php
@@ -124,6 +124,92 @@ $(document).ready(function() {
     </div>
 </div>
 
+<div class="home-ervaringen" style="background-image: linear-gradient( rgb(255 255 255 / 57%), rgb(255 255 255 / 54%) ), url(<?php the_field('achtergrond_afbeelding-ervaringenHome'); ?>)">
+    <div class="home-ervaringen-inner">
+        <h2 class="titel-ervaringenHome"><?php the_field( 'titel-ervaringenHome' ); ?></h2>
+        <?php $knop_ervaringenHome = get_field( 'knop-ervaringenHome' ); ?>
+        <?php if ( $knop_ervaringenHome ) : ?>
+            <div class="knopErvaringenHome">
+                <a class="knop-ervaringenHome knopBruiloft" href="<?php echo esc_url( $knop_ervaringenHome['url'] ); ?>" target="<?php echo esc_attr( $knop_ervaringenHome['target'] ); ?>"><?php echo esc_html( $knop_ervaringenHome['title'] ); ?></a>
+            </div>
+        <?php endif; ?>
+            <div class="ervaringenHome">
+            <?php if ( have_rows( 'ervaringen-repeaterHome' ) ) : ?>
+                <?php while ( have_rows( 'ervaringen-repeaterHome' ) ) : the_row(); ?>
+                    <?php if ( have_rows( 'groupHome-ervaring' ) ) : ?>
+                        <?php while ( have_rows( 'groupHome-ervaring' ) ) : the_row(); ?>
+                            <div class="ervaring-single">
+                                <?php if ( get_sub_field( 'GroupHome-afbeelding' ) ) : ?>
+                                    <img class="ervaring-single-img" src="<?php the_sub_field( 'GroupHome-afbeelding' ); ?>" />
+                                <?php endif ?>
+                                <h3 class="ervaring-single-naam"><?php the_sub_field( 'GroupHome-namen' ); ?></h3>
+                                <div class="ervaring-single-info"><?php the_sub_field( 'GroupHome-informatie' ); ?></div>
+                            </div>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <?php // No rows found ?>
+            <?php endif; ?>
+        </div>
+    </div>
+    <hr class="witteLijn-achtergrond" style="background-image: linear-gradient( rgb(0 0 0 / 0%), rgb(0 0 0 / 0%) ), url(<?php the_field('achtergrond_afbeelding_wit-ervaringenHome'); ?>)">
+</div>
+
+<div class="home-cta" style="background-image: linear-gradient( rgb(0 0 0 / 60%), rgb(0 0 0 / 60%) ), url(<?php the_field('achtergrond_afbeelding-ctaHome'); ?>)">
+    <div class="home-cta-inner">
+        <div class="home-cta-info">
+            <h2 class="titel-home-cta"><?php the_field( 'titel-home-cta' ); ?></h2>
+            <div class="div-knop-ctaHome">
+                <?php $knop_ctaHome = get_field( 'knop-ctaHome' ); ?>
+                <?php if ( $knop_ctaHome ) : ?>
+                    <a class="knopBruiloft knopCtaHome" href="<?php echo esc_url( $knop_ctaHome['url'] ); ?>" target="<?php echo esc_attr( $knop_ctaHome['target'] ); ?>"><?php echo esc_html( $knop_ctaHome['title'] ); ?></a>
+                <?php endif; ?>
+            </div>
+        </div>
+        <div class="home-cta-images">
+            <?php if ( have_rows( 'afbeeldingen-ctaHome' ) ) : ?>
+                <?php while ( have_rows( 'afbeeldingen-ctaHome' ) ) : the_row(); ?>
+                    <?php if ( get_sub_field( 'afbeelding-homeCta_1' ) ) : ?>
+                        <img class="img-cta-home img-cta-home-1" src="<?php the_sub_field( 'afbeelding-homeCta_1' ); ?>" />
+                    <?php endif ?>
+                    <?php if ( get_sub_field( 'afbeelding-homeCta_2' ) ) : ?>
+                        <img class="img-cta-home img-cta-home-2" src="<?php the_sub_field( 'afbeelding-homeCta_2' ); ?>" />
+                    <?php endif ?>
+                    <?php if ( get_sub_field( 'afbeelding-homeCta_3' ) ) : ?>
+                        <img class="img-cta-home img-cta-home-3" src="<?php the_sub_field( 'afbeelding-homeCta_3' ); ?>" />
+                    <?php endif ?>
+                    <?php if ( get_sub_field( 'afbeelding-homeCta_4' ) ) : ?>
+                        <img class="img-cta-home img-cta-home-4" src="<?php the_sub_field( 'afbeelding-homeCta_4' ); ?>" />
+                    <?php endif ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
+<div class="home-insta" style="background-image: linear-gradient( rgb(0 0 0 / 0%), rgb(0 0 0 / 0%) ), url(<?php the_field('achtergrond_afbeelding-instaHome'); ?>)">
+    <div class="home-insta-inner">
+        <h2 class="titel-instaHome"><?php the_field( 'titel-instaHome' ); ?></h2>
+        <div class="fotos-instaHome">
+            <?php if ( have_rows( 'fotos_instagram-instaHome' ) ) : ?>
+                <?php while ( have_rows( 'fotos_instagram-instaHome' ) ) : the_row(); ?>
+                    <?php if ( get_sub_field( 'afbeelding-homeInsta' ) ) : ?>
+                        <img class="instagram-foto-home" src="<?php the_sub_field( 'afbeelding-homeInsta' ); ?>" />
+                    <?php endif ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <?php // No rows found ?>
+            <?php endif; ?>
+        </div>
+        <div class="knopInstaDiv-home">
+            <?php $knop_homeInsta = get_field( 'knop-homeInsta' ); ?>
+            <?php if ( $knop_homeInsta ) : ?>
+                <a class="knopBruiloft knopInsta" href="<?php echo esc_url( $knop_homeInsta['url'] ); ?>" target="<?php echo esc_attr( $knop_homeInsta['target'] ); ?>"><?php echo esc_html( $knop_homeInsta['title'] ); ?></a>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
 
 
 <?php get_footer(); ?>
